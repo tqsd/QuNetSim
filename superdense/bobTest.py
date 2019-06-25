ccd4af920d65b53dd8c4b96f39b1b0451f5c8ff0
Stephen Diadamo <stephen.diadamo@gmail.com>
2019-06-25 13:24:38 +0200

    Deleting duplicate files and code formatting

--- superdense/bobTest.py
+++ superdense/bobTest.py
@@ -1,25 +1,44 @@
 from cqc.pythonLib import CQCConnection, qubit
 
 
-def main():
+def decode(qA, qB):
+    """
+    Return the message encoded into qA with the support of qB.
 
+    Params:
+    qA -- the qubit the message is encoded into
+    qB -- the supporting entangled pair
+
+    """
+
+    qA.cnot(qB)
+    qA.H()
+
+    # Measure
+    a = qA.measure()
+    b = qB.measure()
+
+    return str(a) + str(b)
+
+
+def main():
     # Initialize the connection
     with CQCConnection("Bob") as Bob:
-        
-        qB=Bob.recvQubit()
-        qA=Bob.recvQubit()
+        # Receive qubit from Eve
+        qB = Bob.recvQubit()
 
-        qA.cnot(qB)
-        qA.H()
+        # Receive qubit from Alice
+        qA = Bob.recvQubit()
 
-        a=qA.measure()
-        b=qB.measure()
+        # Decode message
+        message = decode(qA, qB)
 
-        to_print = "App {}: Measurement outcomes are: a={}, b={}".format(Bob.name, a, b)
+        # Print the output of the super dense coding transmission
+        to_print = "Receiver {}: The message Alice sent was: {}".format(Bob.name, message)
         print("|" + "-" * (len(to_print) + 2) + "|")
         print("| " + to_print + " |")
         print("|" + "-" * (len(to_print) + 2) + "|")
 
+
 ##################################################################################################
 main()
-
--- superdense/eveTest.py
+++ superdense/eveTest.py
@@ -2,18 +2,25 @@ from cqc.pythonLib import CQCConnection, qubit
 
 
 def main():
+	# Here Eve acts acts and the entanglement distributor.
+	# To start the protocol, Eve sends one half of an entangled pair to Alice
+	# and the other to Bob.
 
     # Initialize the connection
     with CQCConnection("Eve") as Eve:
-
         qA = qubit(Eve)
         qB = qubit (Eve)
+
+        # Create entanglement between the qubits
         qA.H()
-        qA.cnot(qB)	
+        qA.cnot(qB)
+
+        # Send one half to Alice
         Eve.sendQubit(qA, "Alice")
+
+        # Send one half to Bob
         Eve.sendQubit(qB,"Bob")
 
-	    
 
 ##################################################################################################
 main()
