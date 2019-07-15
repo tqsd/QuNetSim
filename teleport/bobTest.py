c20e8cf9170d3882ea0aa7afcd6d2ccfc6ba0180
Stephen Diadamo <stephen.diadamo@gmail.com>
2019-07-15 11:57:17 +0200

    Separating teleport process to a function

--- teleport/bobTest.py
+++ teleport/bobTest.py
@@ -1,4 +1,5 @@
 from cqc.pythonLib import CQCConnection
+from functions import receive_teleport
 
 
 #####################################################################################################
@@ -8,28 +9,9 @@ from cqc.pythonLib import CQCConnection
 def main():
     # Initialize the connection
     with CQCConnection("Bob") as Bob:
-
-        # Make an EPR pair with Alice
-        qB = Bob.recvEPR()
-
-        # Receive info about corrections
-        data = Bob.recvClassical()
-        message = list(data)
-        a = message[0]
-        b = message[1]
-
-        # Apply corrections
-        if b == 1:
-            qB.X()
-        if a == 1:
-            qB.Z()
-
-            # Measure qubit
+        qB = receive_teleport(Bob)
         m = qB.measure()
-        to_print = "App {}: Measurement outcome is: {}".format(Bob.name, m)
-        print("|" + "-" * (len(to_print) + 2) + "|")
-        print("| " + to_print + " |")
-        print("|" + "-" * (len(to_print) + 2) + "|")
+        print("Measurement outcome: ", m)
 
 
 ##################################################################################################
