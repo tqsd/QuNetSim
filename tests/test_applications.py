de8c0dd7e93f491791a51727d4f3b830aba6f7b5
Stephen Diadamo <stephen.diadamo@gmail.com>
2019-09-02 10:07:52 +0200

    Small changes to the tests and starting a new set of tests for 2 hops

--- tests/test_applications.py
+++ tests/test_applications.py
@@ -14,8 +14,9 @@ from components.network import Network
 do_sequence = True
 
 
-class TestApplications(unittest.TestCase):
+class TestOneHop(unittest.TestCase):
     sim_network = None
+    network = None
 
     @classmethod
     def setUpClass(cls):
@@ -24,25 +25,26 @@ class TestApplications(unittest.TestCase):
         cls.sim_network = SimulaNetwork(nodes=nodes, force=True)
         cls.sim_network.start()
 
+        cls.network = Network.get_instance()
+        cls.network.start()
+
     @classmethod
     def tearDownClass(cls):
         if cls.sim_network is not None:
             cls.sim_network.stop()
         simulaqron_settings.default_settings()
-        #os.system('simulaqron stop')
+        cls.network.stop()
 
     def setUp(self):
-        self.network = Network.get_instance()
-        self.network.start()
+        pass
 
     def tearDown(self):
         for key in self.hosts.keys():
             self.hosts[key].cqc.flush()
             self.hosts[key].stop()
             self.network.remove_host(self.hosts[key])
-        #self.network.stop()
 
-    #@unittest.skip('')
+    # @unittest.skip('')
     def test_send_classical(self):
         with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
             hosts = {'alice': Host('00000000', Alice),
@@ -64,13 +66,12 @@ class TestApplications(unittest.TestCase):
             time.sleep(2)
 
             messages = hosts['bob'].get_classical_messages()
-            Alice.flush()
-            Bob.flush()
+
             self.assertTrue(len(messages) > 0)
             self.assertEqual(messages[0], {'sender': hosts['alice'].host_id,
                                            'message': 'hello'})
 
-    #@unittest.skip('')
+    # @unittest.skip('')
     def test_one_hop_epr(self):
         with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
             hosts = {'alice': Host('00000000', Alice),
@@ -95,11 +96,10 @@ class TestApplications(unittest.TestCase):
             time.sleep(5)
 
             q2 = hosts['bob'].get_epr(hosts['alice'].host_id, q_id)
-            Alice.flush_factory(1, do_sequence)
-            Bob.flush_factory(1, do_sequence)
+
             self.assertEqual(q1.measure(), q2.measure())
 
-    #@unittest.skip('')
+    # @unittest.skip('')
     def test_one_hop_teleport(self):
         with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
             hosts = {'alice': Host('00000000', Alice),
@@ -124,12 +124,11 @@ class TestApplications(unittest.TestCase):
             time.sleep(2)
 
             q2 = hosts['bob'].get_data_qubit(hosts['alice'].host_id)
-            Alice.flush()
-            Bob.flush()
+
             self.assertIsNotNone(q2)
             self.assertEqual(q2['q'].measure(), 1)
 
-    #@unittest.skip('')
+    # @unittest.skip('')
     def test_one_hop_superdense(self):
         with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
             hosts = {'alice': Host('00000000', Alice),
@@ -150,8 +149,148 @@ class TestApplications(unittest.TestCase):
             time.sleep(3)
 
             messages = hosts['bob'].get_classical_messages()
-            Alice.flush()
-            Bob.flush()
+
+            self.assertTrue(len(messages) > 0)
+            self.assertEqual(messages[0], {'sender': hosts['alice'].host_id,
+                                           'message': '01'})
+
+
+@unittest.skip('')
+class TestTwoHop(unittest.TestCase):
+    sim_network = None
+    network = None
+
+    @classmethod
+    def setUpClass(cls):
+        simulaqron_settings.default_settings()
+        nodes = ['Alice', 'Bob', 'Eve']
+        cls.sim_network = SimulaNetwork(nodes=nodes, force=True)
+        cls.sim_network.start()
+
+        cls.network = Network.get_instance()
+        cls.network.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.sim_network is not None:
+            cls.sim_network.stop()
+        simulaqron_settings.default_settings()
+        cls.network.stop()
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        for key in self.hosts.keys():
+            self.hosts[key].cqc.flush()
+            self.hosts[key].stop()
+            self.network.remove_host(self.hosts[key])
+
+    # @unittest.skip('')
+    def test_send_classical(self):
+        with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
+            hosts = {'alice': Host('00000000', Alice),
+                     'bob': Host('00000001', Bob)}
+
+            self.hosts = hosts
+            # A <-> B
+
+            hosts['alice'].add_connection('00000001')
+
+            hosts['alice'].start()
+            hosts['bob'].start()
+
+            for h in hosts.values():
+                self.network.add_host(h)
+
+            hosts['alice'].send_classical(hosts['bob'].host_id, 'hello')
+
+            time.sleep(2)
+
+            messages = hosts['bob'].get_classical_messages()
+
+            self.assertTrue(len(messages) > 0)
+            self.assertEqual(messages[0], {'sender': hosts['alice'].host_id,
+                                           'message': 'hello'})
+
+    # @unittest.skip('')
+    def test_one_hop_epr(self):
+        with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
+            hosts = {'alice': Host('00000000', Alice),
+                     'bob': Host('00000001', Bob)}
+
+            self.hosts = hosts
+
+            # A <-> B
+            hosts['alice'].add_connection('00000001')
+
+            hosts['alice'].start()
+            hosts['bob'].start()
+
+            for h in hosts.values():
+                self.network.add_host(h)
+
+            q_id = hosts['alice'].send_epr(hosts['bob'].host_id)
+            time.sleep(5)
+
+            q1 = hosts['alice'].get_epr(hosts['bob'].host_id, q_id)
+
+            time.sleep(5)
+
+            q2 = hosts['bob'].get_epr(hosts['alice'].host_id, q_id)
+
+            self.assertEqual(q1.measure(), q2.measure())
+
+    # @unittest.skip('')
+    def test_one_hop_teleport(self):
+        with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
+            hosts = {'alice': Host('00000000', Alice),
+                     'bob': Host('00000001', Bob)}
+
+            self.hosts = hosts
+
+            # A <-> B
+            hosts['alice'].add_connection('00000001')
+
+            hosts['alice'].start()
+            hosts['bob'].start()
+
+            for h in hosts.values():
+                self.network.add_host(h)
+
+            q = qubit(Alice)
+            q.X()
+
+            hosts['alice'].send_teleport(hosts['bob'].host_id, q)
+
+            time.sleep(2)
+
+            q2 = hosts['bob'].get_data_qubit(hosts['alice'].host_id)
+
+            self.assertIsNotNone(q2)
+            self.assertEqual(q2['q'].measure(), 1)
+
+    # @unittest.skip('')
+    def test_one_hop_superdense(self):
+        with CQCConnection("Alice") as Alice, CQCConnection("Bob") as Bob:
+            hosts = {'alice': Host('00000000', Alice),
+                     'bob': Host('00000001', Bob)}
+            self.hosts = hosts
+
+            # A <-> B
+            hosts['alice'].add_connection('00000001')
+
+            hosts['alice'].start()
+            hosts['bob'].start()
+
+            for h in hosts.values():
+                self.network.add_host(h)
+
+            hosts['alice'].send_superdense(hosts['bob'].host_id, '01')
+
+            time.sleep(3)
+
+            messages = hosts['bob'].get_classical_messages()
 
             self.assertTrue(len(messages) > 0)
             self.assertEqual(messages[0], {'sender': hosts['alice'].host_id,
