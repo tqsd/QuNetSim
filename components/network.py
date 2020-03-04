27faeb6c46c219cbebdf74ea6ed27d67c7a92fdc
Benjamin <benjamin.zanger@gmx.de>
2020-03-04 14:45:09 +0100

    release qubit if it is lost by a packet drop

--- components/network.py
+++ components/network.py
@@ -528,6 +528,9 @@ class Network:
                 packet_drop_var = random.random()
                 if packet_drop_var > (1 - self.packet_drop_rate):
                     Logger.get_instance().log("PACKET DROPPED")
+                    if packet.payload_type == protocols.QUANTUM:
+                        packet.payload.release()
+                        print("Qubit was released")
                     continue
 
                 sender, receiver = packet.sender, packet.receiver
