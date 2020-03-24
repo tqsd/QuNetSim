8bdafe0131997d46b120e4c2f54860572e461b36
Stephen Diadamo <stephen.diadamo@gmail.com>
2020-03-24 16:37:39 +0100

    Updating pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='qunetsim',
-    version='0.0.10',
+    version='0.0.11',
     scripts=['bin/template'],
     author="Stephen Diadamo",
     author_email="stephen.diadamo@gmail.com",
