5249689583951c43a55ae62e9e2cb4d3a06c2d63
Stephen Diadamo <stephen.diadamo@gmail.com>
2020-04-03 11:45:24 +0200

    Reorganizing new examples

--- examples/send_classical/send_classical_no_ack.py
+++ examples/send_classical/send_classical_no_ack.py
@@ -1,8 +1,3 @@
-from cqc.pythonLib import CQCConnection
-import sys
-import time
-
-sys.path.append("../..")
 from components.host import Host
 from backends.cqc_backend import CQCBackend
 from components.network import Network
@@ -10,38 +5,28 @@ from components.network import Network
 
 def main():
     network = Network.get_instance()
-    nodes = ["Alice", "Bob", "Eve", "Dean"]
-    backend = CQCBackend()
-    network.start(nodes, backend)
+    network.start()
     network.delay = 0.2
-    network.packet_drop_rate = 0
-    print('')
 
     host_alice = Host('Alice', backend)
     host_alice.add_connection('Bob')
     host_alice.start()
 
     host_bob = Host('Bob', backend)
-    host_bob.add_connection('Alice')
-    host_bob.add_connection('Eve')
+    host_bob.add_connections(['Alice', 'Eve'])
     host_bob.start()
 
     host_eve = Host('Eve', backend)
-    host_eve.add_connection('Bob')
-    host_eve.add_connection('Dean')
+    host_eve.add_connections(['Bob', 'Dean'])
     host_eve.start()
 
     host_dean = Host('Dean', backend)
     host_dean.add_connection('Eve')
     host_dean.start()
 
-    network.add_host(host_alice)
-    network.add_host(host_bob)
-    network.add_host(host_eve)
-    network.add_host(host_dean)
+    network.add_hosts([host_alice, host_bob, host_eve, host_dean])
 
     print('alice sends message')
-
     host_alice.send_classical('Bob', 'hello1', no_ack=True)
     host_alice.send_classical('Bob', 'hello2', no_ack=True)
     host_alice.send_classical('Bob', 'hello3', no_ack=True)
