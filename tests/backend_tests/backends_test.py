9871ddda1b36f90df5262334e1ffc8d3d26c86bd
Stephen DiAdamo <stephen.diadamo@riverlane.com>
2020-06-16 10:58:18 +0100

    Fixing import statement...

--- tests/backend_tests/backends_test.py
+++ tests/backend_tests/backends_test.py
@@ -1,6 +1,6 @@
 from qunetsim.components.network import Network
 from qunetsim.components.host import Host
-from qunetsim.backends import CQCBackend
+from qunetsim.backends.cqc_backend import CQCBackend
 import sys
 
 sys.path.append("../..")
