4f65d64297dd77097a7e37604b878de88a4e6e0c
Benjamin Zanger <benjamin.zanger@gmx.de>
2020-08-11 13:42:19 +0200

    added ProjectQ backend to backends

--- qunetsim/backends/__init__.py
+++ qunetsim/backends/__init__.py
@@ -1,4 +1,5 @@
 from .eqsn_backend import EQSNBackend
 from .cqc_backend import CQCBackend
+from .projectq_backend import ProjectQBackend
 from .rw_lock import RWLock
 from .safe_dict import SafeDict
