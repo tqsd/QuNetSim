05e079d0a3a4916f699c31098e7257155374f7e9
Stephen Diadamo <stephen.diadamo@gmail.com>
2020-11-12 11:57:09 +0100

    Fixing requiring optional backends

--- qunetsim/backends/__init__.py
+++ qunetsim/backends/__init__.py
@@ -1,5 +1,19 @@
+# Default backend
 from .eqsn_backend import EQSNBackend
-from .cqc_backend import CQCBackend
-from .qutip_backend import QuTipBackend
+
+# Optional backends
+try:
+    from .cqc_backend import CQCBackend
+except RuntimeError:
+    pass
+try:
+    from .qutip_backend import QuTipBackend
+except RuntimeError:
+    pass
+try:
+    from .projectq_backend import ProjectQBackend
+except RuntimeError:
+    pass
+
 from .rw_lock import RWLock
 from .safe_dict import SafeDict
