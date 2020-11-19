32162219571d59829f1b7f0f6e5b2949f6f07631
Benjamin Zanger <benjamin.zanger@gmx.de>
2020-11-19 20:47:23 +0100

    first commit for emulated backend

---
+++ qunetsim/backends/emulated_backend.py
@@ -0,0 +1,318 @@
+from qunetsim.backends.rw_lock import RWLock
+from qunetsim.objects.qubit import Qubit
+import enum
+try:
+    import serial
+except ImportError:
+    raise RuntimeError(" To use the Emulated Backend you need to install "
+                       "pyserial!")
+
+
+class Commands(enum.Enum):
+    IDLE = 0
+    APPLY_GATE = 1
+    MEASURE = 2
+    NEW_QUBIT = 3
+    SEND_QUBIT = 4
+    CREATE_ENTANGLED_PAIR = 5
+
+
+def create_data_frame(command, qubit1_id=None, qubit2_id=None):
+    pass
+
+
+class EmulationBackend(object):
+    """
+    Backend which connects to a Quantum Networking Card.
+    """
+
+    def __init__(self):
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def start(self, **kwargs):
+        """
+        Starts Backends which have to run in an own thread or process before they
+        can be used.
+        """
+        raise EnvironmentError("This is only an interface, not \
+                        an actual implementation!")
+
+    def stop(self):
+        """
+        Stops Backends which are running in an own thread or process.
+        """
+        raise EnvironmentError("This is only an interface, not \
+                        an actual implementation!")
+
+    def add_host(self, host):
+        """
+        Adds a host to the backend.
+
+        Args:
+            host (Host): New Host which should be added.
+        """
+        raise EnvironmentError("This is only an interface, not \
+                        an actual implementation!")
+
+    def create_qubit(self, host_id):
+        """
+        Creates a new Qubit of the type of the backend.
+
+        Args:
+            host_id (String): Id of the host to whom the qubit belongs.
+
+        Reurns:
+            Qubit of backend type.
+        """
+        raise EnvironmentError("This is only an interface, not \
+                        an actual implementation!")
+
+    def send_qubit_to(self, qubit, from_host_id, to_host_id):
+        """
+        Sends a qubit to a new host.
+
+        Args:
+            qubit (Qubit): Qubit to be send.
+            from_host_id (String): From the starting host.
+            to_host_id (String): New host of the qubit.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def create_EPR(self, host_a_id, host_b_id, q_id=None, block=False):
+        """
+        Creates an EPR pair for two qubits and returns one of the qubits.
+
+        Args:
+            host_a_id (String): ID of the first host who gets the EPR state.
+            host_b_id (String): ID of the second host who gets the EPR state.
+            q_id (String): Optional id which both qubits should have.
+            block (bool): Determines if the created pair should be blocked or not.
+        Returns:
+            Returns a qubit. The qubit belongs to host a. To get the second
+            qubit of host b, the receive_epr function has to be called.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def receive_epr(self, host_id, sender_id, q_id=None, block=False):
+        """
+        Called after create EPR in the receiver, to receive the other EPR pair.
+
+        Args:
+            host_id (String): ID of the first host who gets the EPR state.
+            sender_id (String): ID of the sender of the EPR pair.
+            q_id (String): Optional id which both qubits should have.
+            block (bool): Determines if the created pair should be blocked or not.
+        Returns:
+            Returns an EPR qubit with the other Host.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    ##########################
+    #   Gate definitions    #
+    #########################
+
+    def I(self, qubit):
+        """
+        Perform Identity gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def X(self, qubit):
+        """
+        Perform pauli X gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def Y(self, qubit):
+        """
+        Perform pauli Y gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def Z(self, qubit):
+        """
+        Perform pauli Z gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def H(self, qubit):
+        """
+        Perform Hadamard gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def T(self, qubit):
+        """
+        Perform T gate on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def rx(self, qubit, phi):
+        """
+        Perform a rotation pauli x gate with an angle of phi.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+            phi (float): Amount of rotation in Rad.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def ry(self, qubit, phi):
+        """
+        Perform a rotation pauli y gate with an angle of phi.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+            phi (float): Amount of rotation in Rad.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def rz(self, phi):
+        """
+        Perform a rotation pauli z gate with an angle of phi.
+
+        Args:
+            qubit (Qubit): Qubit on which gate should be applied to.
+            phi (float): Amount of rotation in Rad.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def cnot(self, qubit, target):
+        """
+        Applies a controlled x gate to the target qubit.
+
+        Args:
+            qubit (Qubit): Qubit to control cnot.
+            target (Qubit): Qubit on which the cnot gate should be applied.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def cphase(self, qubit, target):
+        """
+        Applies a controlled z gate to the target qubit.
+
+        Args:
+            qubit (Qubit): Qubit to control cphase.
+            target (Qubit): Qubit on which the cphase gate should be applied.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def custom_gate(self, qubit, gate):
+        """
+        Applies a custom gate to the qubit.
+
+        Args:
+            qubit(Qubit): Qubit to which the gate is applied.
+            gate(np.ndarray): 2x2 array of the gate.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def custom_controlled_gate(self, qubit, target, gate):
+        """
+        Applies a custom gate to the target qubit, controlled by the qubit.
+
+        Args:
+            qubit(Qubit): Qubit to control the gate.
+            target(Qubit): Qubit on which the gate is applied.
+            gate(nd.array): 2x2 array for the gate applied to target.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def custom_controlled_two_qubit_gate(self, qubit, target_1, target_2, gate):
+        """
+        Applies a custom gate to the target qubit, controlled by the qubit.
+
+        Args:
+            qubit (Qubit): Qubit to control the gate.
+            target_1 (Qubit): Qubit on which the gate is applied.
+            target_2 (Qubit): Qubit on which the gate is applied.
+            gate (nd.array): 4x4 array for the gate applied to target.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def custom_two_qubit_gate(self, qubit1, qubit2, gate):
+        """
+        Applies a custom two qubit gate to qubit1 \\otimes qubit2.
+
+        Args:
+            qubit1(Qubit): First qubit of the gate.
+            qubit2(Qubit): Second qubit of the gate.
+            gate(np.ndarray): 4x4 array for the gate applied.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def density_operator(self, qubit):
+        """
+        Returns the density operator of this qubit. If the qubit is entangled,
+        the density operator will be in a mixed state.
+
+        Args:
+            qubit (Qubit): Qubit of the density operator.
+
+        Returns:
+            np.ndarray: The density operator of the qubit.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def measure(self, qubit, non_destructive):
+        """
+        Perform a measurement on a qubit.
+
+        Args:
+            qubit (Qubit): Qubit which should be measured.
+            non_destructive (bool): Determines if the Qubit should stay in the
+                                    system or be eliminated.
+
+        Returns:
+            The value which has been measured.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
+
+    def release(self, qubit):
+        """
+        Releases the qubit.
+
+        Args:
+            qubit (Qubit): The qubit which should be released.
+        """
+        raise (EnvironmentError("This is only an interface, not \
+                        an actual implementation!"))
