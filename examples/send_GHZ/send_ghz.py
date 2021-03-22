90d9bf2fd61f5b55276415a32582159698f1fc29
Stephen Diadamo <stephen.diadamo@gmail.com>
2021-03-22 17:31:54 +0100

    Updating benchmarks

--- examples/send_GHZ/send_ghz.py
+++ examples/send_GHZ/send_ghz.py
@@ -39,31 +39,32 @@ def main():
     network.add_host(host_eve)
     network.add_host(host_dean)
 
-    share_list = ["Bob", "Eve", "Dean"]
-    q_id1, ack_received = host_alice.send_ghz(share_list, await_ack=True)
+    for i in range(10):
+        share_list = ["Bob", "Eve", "Dean"]
+        q_id1, ack_received = host_alice.send_ghz(share_list, await_ack=False, no_ack=True)
 
-    print("Alice received ACK from all? " + str(ack_received))
+        print("Alice received ACK from all? " + str(ack_received))
 
-    q1 = host_alice.get_ghz('Alice', q_id1, wait=10)
-    q2 = host_bob.get_ghz('Alice', q_id1, wait=10)
-    q3 = host_eve.get_ghz('Alice', q_id1, wait=10)
-    q4 = host_dean.get_ghz('Alice', q_id1, wait=10)
+        q1 = host_alice.get_ghz('Alice', q_id1, wait=10)
+        q2 = host_bob.get_ghz('Alice', q_id1, wait=10)
+        q3 = host_eve.get_ghz('Alice', q_id1, wait=10)
+        q4 = host_dean.get_ghz('Alice', q_id1, wait=10)
 
-    if q1 is None:
-        raise ValueError("Q1 is none")
-    if q2 is None:
-        raise ValueError("Q2 is none")
-    if q3 is None:
-        raise ValueError("Q3 is none")
-    if q4 is None:
-        raise ValueError("Q4 is none")
+        if q1 is None:
+            raise ValueError("Q1 is none")
+        if q2 is None:
+            raise ValueError("Q2 is none")
+        if q3 is None:
+            raise ValueError("Q3 is none")
+        if q4 is None:
+            raise ValueError("Q4 is none")
 
-    m1 = q1.measure()
-    m2 = q2.measure()
-    m3 = q3.measure()
-    m4 = q4.measure()
+        m1 = q1.measure()
+        m2 = q2.measure()
+        m3 = q3.measure()
+        m4 = q4.measure()
 
-    print("results of measurements are %d, %d, %d, and %d." % (m1, m2, m3, m4))
+        print("results of measurements are %d, %d, %d, and %d." % (m1, m2, m3, m4))
 
     network.stop(True)
     exit()
