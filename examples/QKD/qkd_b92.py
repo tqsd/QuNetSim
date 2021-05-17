3092f3e26ee3f73c1a92aa202a1260aac1109883
sashofthedark <sasha.bakman@gmail.com>
2021-05-17 15:22:22 +0300

    added structure including sniffing

--- examples/QKD/qkd_b92.py
+++ examples/QKD/qkd_b92.py
@@ -4,6 +4,7 @@ import numpy as np
 from qunetsim.components import Host, Network
 from qunetsim.objects import Qubit, Logger
 from qunetsim.backends import EQSNBackend
+from random import randint
 
 Logger.DISABLED = True
 
@@ -35,6 +36,9 @@ def build_network_b92():
     network.add_host(host_alice)
     network.add_host(host_bob)
     network.add_host(host_eve)
+
+    host_eve.quantum_relay_sniffing = True
+    host_eve.set_quantum_relay_sniffing_function(eve_sniffing_quantum)
     
     hosts = [host_alice,host_bob,host_eve]
 
@@ -43,14 +47,38 @@ def build_network_b92():
 
 def generate_key(key_length):
     #generate an encrypted key of a certain length
+    generated_key = []
+    for i in range(key_length):
+        generated_key.append(randint(0,1))
+
+def sender_qkd(host, msg_buff, secret_key, receiver):
+    #Alice sends the key to Bob
+    #the key is an array of binary numbers
     pass
 
-def send_key(key):
-    #Alice sends the key to Bob
+def receiver_qkd(host, msg_buff, key_size, sender):
     pass
+
+def check_key_sender(host, msg_buff, lenght_of_check, receiver):
+    pass
+
+def check_key_receiver(host, msg_buff, length_of_check,sender):
+    pass
+
+def eve_sniffing_quantum(sender,receiver,qubit):
+    base = randint(0,1)
+    if base == 1:
+        qubit.H()
+        #measure the qubit in the diagonal basis basis
+        qubit.measure()
+    elif base == 0:
+        #measure the qubit in the rectilinear basis
+        qubit.measure()
+
 
 def b92_protocol():
-    network, nosts = build_network_b92()
+    network, hosts = build_network_b92()
+    thread_1 = hosts[0].run_protocol()
 
 def main():
     #run the b92 protocol function
