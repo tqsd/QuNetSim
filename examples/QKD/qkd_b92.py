7c8e97929be5aba60b0d016812f9653510911776
sashofthedark <sasha.bakman@gmail.com>
2021-05-19 23:26:28 +0300

    the program is running now without errors

--- examples/QKD/qkd_b92.py
+++ examples/QKD/qkd_b92.py
@@ -66,10 +66,9 @@ def generate_key(key_length):
     print(f'Generated the key {generated_key}')
     return generated_key
 
-def sender_qkd(alice, msg_buff, secret_key, receiver):
+def sender_qkd(alice, secret_key, receiver):
     #Alice sends the key to Bob
     #the key is an array of binary numbers
-    sequence_nr = 0
     sent_qubit_counter = 0
     for bit in secret_key:
         success = False
@@ -80,20 +79,19 @@ def sender_qkd(alice, msg_buff, secret_key, receiver):
             #there is no random choice of basis here.
             #If we want to send 0, we'll send |0>
             #If we want to send 1, we'll send |+>
-            alice.send_qubit(receiver, qubit, await_ack = True)
+            _, ack = alice.send_qubit(receiver, qubit, await_ack = True)
+            if ack is not None:
+                 print('Alice\'s qubit successfully sent')
             #get a message from Bob whether he measured a "1" in rectilinear basis or a "-" in diagonal basis
-            message = alice.get_classical(receiver, seq_num = sequence_nr, wait = wait_time)
-            while message == None:
-                message = alice.get_classical(receiver, seq_num = sequence_nr, wait = wait_time)
-            print(f'message is {message}')
+            message = alice.get_classical(receiver, wait = wait_time)
+            if message is not None:
 
-            if message.content == 'qubit successfully acquired':
-                success = True
-                sent_qubit_counter += 1
-                print(f'Sent a qubit {sent_qubit_counter} to Bob successfully!')
+                if message[0].content == 'qubit successfully acquired':
+                    success = True
+                    sent_qubit_counter += 1
+                    print(f'Sent a qubit {sent_qubit_counter} to Bob successfully!')
 
                 #and we can move on and send the next qubit
-            sequence_nr += 1
         
 def receiver_qkd(bob, key_size, sender):
     sequence_nr = 0
@@ -123,24 +121,24 @@ def receiver_qkd(bob, key_size, sender):
                 resulting_key_bit = 1
             message_to_send = 'qubit successfully acquired'
             key_array.append(resulting_key_bit)  
-        bob.send_classical(sender,message_to_send, await_ack = True)  
-        sequence_nr += 1 #total counter of transactions
+        bob_sent = bob.send_classical(sender,message_to_send, await_ack = True) 
+        if bob_sent is not None:
+            print('Bob successfully sent a message to Alice')
+            sequence_nr += 1 #total counter of transactions
     return key_array
 
-def check_key_sender(alice, msg_buff, key_check_alice, receiver):
+def check_key_sender(alice, key_check_alice, receiver):
     for bit in key_check_alice:
-        sequence_nr = 0
         cntr = 0
         eavesdropping = False
-        msg_buff = []
         len_check = len(key_check_alice)
         while (cntr < len_check) and (eavesdropping == False):
             alice.send_classical(receiver, key_check_alice[cntr], await_ack = True)
-            message_from_bob = alice.get_classical(receiver,seq_num = sequence_nr, wait = wait_time)
-            if message_from_bob.content == 'fail':
-                eavesdropping = True
-            cntr += 1
-            sequence_nr += 1
+            message_from_bob = alice.get_classical(receiver, wait = wait_time)
+            if message_from_bob is not None:
+                if message_from_bob[0].content == 'fail':
+                    eavesdropping = True
+                cntr += 1
         if eavesdropping == False:
             return 'key successfully checked'
             print('Key successfully checked')
@@ -152,30 +150,28 @@ def check_key_receiver(bob, key_check_bob,sender):
     cntr = 0
     msg_buff = []
     eavesdropping = False
-    sequence_nr = 0
     key_check_length = len(key_check_bob)
     while (cntr < key_check_length) and (eavesdropping == False):
-        bit_from_alice = bob.get_classical(sender, seq_num = sequence_nr, wait = wait_time)
-        if bit_from_alice.content == key_check_bob[cntr]:
-            bob.send_classical(sender,'success',await_ack = True)
-        else:
-            bob.send_classical(sender,'fail',await_ack = True)
-            eavesdropping = True
-        sequence_nr += 1
+        bit_from_alice = bob.get_classical(sender, wait = wait_time)
+        if bit_from_alice is not None:
+            if bit_from_alice[0].content == key_check_bob[cntr]:
+                bob.send_classical(sender,'success',await_ack = True)
+            else:
+                bob.send_classical(sender,'fail',await_ack = True)
+                eavesdropping = True
         cntr += 1
 
 def alice_func(host, bob_id, length_of_check, key_length):
     #generate the encrypted key
     encryption_key_binary = generate_key(key_length)
-    msg_buff = []
     for item in encryption_key_binary:
-        sender_qkd(host, msg_buff, encryption_key_binary, bob_id)
+        sender_qkd(host, encryption_key_binary, bob_id)
 
     print('Sent all the qubits sucessfully!')
 
     key_to_test = encryption_key_binary[0:(length_of_check-1)]
     print(f'Alice\'s key is {key_to_test}')
-    check_key_sender(host, msg_buff, key_to_test ,bob_id)
+    check_key_sender(host, key_to_test ,bob_id)
         #what do these function return, if at all? 
 
 def bob_func(host, alice_id, length_of_check, key_length):
@@ -192,11 +188,11 @@ def b92_protocol(eve_interception, key_length, length_of_check):
     host_alice = hosts[0]
     host_bob = hosts[1]
     bob_id = host_bob.host_id
-    thread_1 = host_alice.run_protocol(alice_func,(bob_id, length_of_check, key_length))
+    alice_id = host_alice.host_id
+    thread_1 = host_alice.run_protocol(alice_func,(bob_id, length_of_check, key_length,))
     #the protocol for Alice and for Bob should receive the length of the encrypted key, 
     # and the instance of the second host
-    alice_id = host_alice.host_id
-    thread_2 = host_bob.run_protocol(bob_func,(alice_id, length_of_check, key_length))
+    thread_2 = host_bob.run_protocol(bob_func,(alice_id, length_of_check, key_length,))
 
     thread_1.join()
     thread_2.join()
@@ -208,6 +204,6 @@ if __name__ == '__main__':
     key_length = 3
     length_of_check = round(key_length/3)
     #the length of the encrypted key
-    eve_interception = True
+    eve_interception = False
     #whether or not Eve eavesdrops on the quantum channel
     b92_protocol(eve_interception, key_length, length_of_check)
