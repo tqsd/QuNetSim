041f94414d229ab09e2e41169d35f1520ddf36d8
sashofthedark <sasha.bakman@gmail.com>
2021-05-20 19:06:36 +0300

    the program works without eavesdropping

--- examples/QKD/qkd_b92.py
+++ examples/QKD/qkd_b92.py
@@ -8,7 +8,7 @@ from qunetsim.objects import Logger
 from random import randint
 
 Logger.DISABLED = True
-wait_time = 5
+wait_time = 60
 
 def eve_sniffing_quantum(sender,receiver,qubit):
     base = randint(0,1)
@@ -67,12 +67,10 @@ def generate_key(key_length):
     return generated_key
 
 def sender_qkd(alice, secret_key, receiver):
-    #Alice sends the key to Bob
-    #the key is an array of binary numbers
     sent_qubit_counter = 0
     for bit in secret_key:
         success = False
-        while not success:
+        while success == False:
             qubit = Qubit(alice)
             if bit == 1:
                 qubit.H()
@@ -80,108 +78,82 @@ def sender_qkd(alice, secret_key, receiver):
             #If we want to send 0, we'll send |0>
             #If we want to send 1, we'll send |+>
             _, ack = alice.send_qubit(receiver, qubit, await_ack = True)
-            if ack is not None:
-                 print('Alice\'s qubit successfully sent')
             #get a message from Bob whether he measured a "1" in rectilinear basis or a "-" in diagonal basis
-            message = alice.get_classical(receiver, wait = wait_time)
+            message = alice.get_next_classical(receiver, wait = -1)
             if message is not None:
-
-                if message[0].content == 'qubit successfully acquired':
+                if message.content == 'qubit successfully acquired':
+                    print(f'Alice sent qubit {sent_qubit_counter+1} to Bob')
                     success = True
                     sent_qubit_counter += 1
-                    print(f'Sent a qubit {sent_qubit_counter} to Bob successfully!')
-
-                #and we can move on and send the next qubit
         
 def receiver_qkd(bob, key_size, sender):
-    sequence_nr = 0
     key_array = []
     received_counter = 0
-    t_wait = 5
 
     while received_counter < key_size:
         #while we haven't received all the bits of the key successfully
         base = randint(0,1)
         #receive qubit from Alice
-        qubit = bob.get_data_qubit(sender,wait = t_wait)
-        
-        while qubit is None:
-            qubit = bob.get_data_qubit(sender, wait = t_wait)
-
-        print(f'Received qubit {sequence_nr} and it\'s of the type {type(qubit)}!')
-        if base == 1:
-            qubit.H()
-        bit = qubit.measure()
-        if bit == 0:
-            message_to_send = 'failed to acquire a qubit'
-        elif bit == 1:
+        qubit = bob.get_data_qubit(sender,wait = wait_time)
+        if qubit is not None:
             if base == 1:
-                resulting_key_bit = 0
-            elif base == 0:
-                resulting_key_bit = 1
-            message_to_send = 'qubit successfully acquired'
-            key_array.append(resulting_key_bit)  
-        bob_sent = bob.send_classical(sender,message_to_send, await_ack = True) 
-        if bob_sent is not None:
-            print('Bob successfully sent a message to Alice')
-            sequence_nr += 1 #total counter of transactions
+                qubit.H()
+            bit = qubit.measure()
+            if bit == 1:
+                if base == 1:
+                    resulting_key_bit = 0
+                elif base == 0:
+                    resulting_key_bit = 1
+                message_to_send = 'qubit successfully acquired'
+                key_array.append(resulting_key_bit) 
+                received_counter += 1 
+                print(f'Bob received qubit {received_counter}')
+            else:
+                message_to_send = 'fail'
+            bob.send_classical(sender,message_to_send, await_ack = True) 
     return key_array
 
+
 def check_key_sender(alice, key_check_alice, receiver):
-    for bit in key_check_alice:
-        cntr = 0
-        eavesdropping = False
-        len_check = len(key_check_alice)
-        while (cntr < len_check) and (eavesdropping == False):
-            alice.send_classical(receiver, key_check_alice[cntr], await_ack = True)
-            message_from_bob = alice.get_classical(receiver, wait = wait_time)
-            if message_from_bob is not None:
-                if message_from_bob[0].content == 'fail':
-                    eavesdropping = True
-                cntr += 1
-        if eavesdropping == False:
-            return 'key successfully checked'
-            print('Key successfully checked')
-        else:
-            return 'key was corrupted by Eve'
-            print('key corrupted by Eve!')
+    key_check_string = ''.join([str(x) for x in key_check_alice])
+    print(f'Alice\'s key to check is {key_check_string}')
+    ack_alice = alice.send_classical(receiver,key_check_string,await_ack = True)
+
+    message_from_bob = alice.get_next_classical(receiver, wait = -1)
+
+    if message_from_bob is not None:
+        if message_from_bob.content == 'Success':
+            print('Key is successfully verified')
+        elif message_from_bob.content == 'Fail':
+            print('Key has been corrupted')
+
 
 def check_key_receiver(bob, key_check_bob,sender):
-    cntr = 0
-    msg_buff = []
-    eavesdropping = False
-    key_check_length = len(key_check_bob)
-    while (cntr < key_check_length) and (eavesdropping == False):
-        bit_from_alice = bob.get_classical(sender, wait = wait_time)
-        if bit_from_alice is not None:
-            if bit_from_alice[0].content == key_check_bob[cntr]:
-                bob.send_classical(sender,'success',await_ack = True)
-            else:
-                bob.send_classical(sender,'fail',await_ack = True)
-                eavesdropping = True
-        cntr += 1
+    key_check_bob_string = ''.join([str(x) for x in key_check_bob])
+    print(f'Bob\'s key to check is {key_check_bob_string}')
+    key_from_alice = bob.get_next_classical(sender, wait = -1)
+    if key_from_alice is not None:
+        print(f'the partial key received from Alice is {key_from_alice.content}')
+        if key_from_alice.content == key_check_bob_string:
+            ack_bob = bob.send_classical(sender,'Success',await_ack = True)
+        else:
+            ack_bob = bob.send_classical(sender,'Fail',await_ack = True)
+
 
 def alice_func(host, bob_id, length_of_check, key_length):
     #generate the encrypted key
     encryption_key_binary = generate_key(key_length)
-    for item in encryption_key_binary:
-        sender_qkd(host, encryption_key_binary, bob_id)
-
+    sender_qkd(host, encryption_key_binary, bob_id)
     print('Sent all the qubits sucessfully!')
-
     key_to_test = encryption_key_binary[0:(length_of_check-1)]
-    print(f'Alice\'s key is {key_to_test}')
     check_key_sender(host, key_to_test ,bob_id)
-        #what do these function return, if at all? 
+
 
 def bob_func(host, alice_id, length_of_check, key_length):
     secret_key_bob = receiver_qkd(host, key_length, alice_id)
-    print(f'Bob\'s key is {secret_key_bob}')
     key_to_test = secret_key_bob[0:(length_of_check-1)]
-    message = check_key_receiver(host, key_to_test, alice_id)
-    print(message)
-    #the message contains whether we successfully transferred the key
-    #or whether eavesdropping occurred
+    check_key_receiver(host, key_to_test, alice_id)
+
 
 def b92_protocol(eve_interception, key_length, length_of_check):
     network, hosts = build_network_b92(eve_interception)
@@ -189,9 +161,8 @@ def b92_protocol(eve_interception, key_length, length_of_check):
     host_bob = hosts[1]
     bob_id = host_bob.host_id
     alice_id = host_alice.host_id
+
     thread_1 = host_alice.run_protocol(alice_func,(bob_id, length_of_check, key_length,))
-    #the protocol for Alice and for Bob should receive the length of the encrypted key, 
-    # and the instance of the second host
     thread_2 = host_bob.run_protocol(bob_func,(alice_id, length_of_check, key_length,))
 
     thread_1.join()
@@ -200,9 +171,10 @@ def b92_protocol(eve_interception, key_length, length_of_check):
     network.stop(True)
     exit()
 
+
 if __name__ == '__main__':
-    key_length = 3
-    length_of_check = round(key_length/3)
+    key_length = 10
+    length_of_check = round(key_length/2)
     #the length of the encrypted key
     eve_interception = False
     #whether or not Eve eavesdrops on the quantum channel
